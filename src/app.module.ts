c1049670c607b248ec0b75a88017a346d307c1d0
Rishal EP <e.rishalep@gmail.com>
2026-02-28 15:04:18 +0400

    Module Initialisation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { BlockchainModule } from './blockchain/blockchain.module';
+import { GasModule } from './gas/gas.module';
+import { UniswapModule } from './uniswap/uniswap.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    BlockchainModule,
+    GasModule,
+    UniswapModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
